c1318f969beb028b8f66ee9329772c13d0f3873c
Денис Сучков <ny1828@yandex.ru>
2020-01-19 19:07:04 +0300

    реализация классов

--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,54 @@
 class Car:
-    # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
-    # Поля должны задаваться через конструктор
-    pass
+    """
+    Класс машины Car, у которого есть поля: марка и модель автомобиля.
+    Поля задаются через конструктор
+    """
+    def __init__(self, mark, model):
+        self.mark = mark
+        self.model = model
+
+    def __str__(self):
+        return f"Машина: {self.mark} - {self.model}"
 
 class Garage:
-    # Написать класс гаража Garage, у которого есть поле списка машин
-    # Поле должно задаваться через конструктор
-    # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
-    # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    """
+    Класс гаража Garage, у которого есть поле списка машин.
+    Поле задается через конструктор.
+    По аналогии с классом Company из лекции реализован интерфейс итерируемого.
+    Реализованы методы add и delete(удалять по индексу) машин из гаража
+    """
+    def __init__(self, cars):
+        if list != type(cars):
+            raise ValueError("Конструктор принимает список!")
+        self.cars = cars
+
+    def __len__(self):
+        return len(self.cars)
+
+    def __getitem__(self, item):
+        return self.cars[item]
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        if len(self.cars) > index:
+            del self.cars[index]
+
+    def __str__(self):
+        return f"Гараж: {self.cars}"
+
+
+if __name__ == '__main__':
+    car = Car("Hyundai", "Elantra")
+    assert car.mark == "Hyundai" and car.model == "Elantra"
+
+    garage = Garage([car])
+    assert len(garage) == 1 and car in garage
+
+    car2 = Car("BMW", "X5")
+    garage.add(car2)
+    assert len(garage) == 2 and car2 in garage
+
+    garage.delete(1)
+    assert len(garage) == 1 and car in garage and car2 not in garage
