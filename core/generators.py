ef41c3c9f0e19d500d59ae9172612a338a1ed48a
Денис Сучков <ny1828@yandex.ru>
2020-01-19 20:12:56 +0300

    реализация генераторов

--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,25 @@
-# написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
-def gen(N):
-    pass
+from random import randint
+from types import GeneratorType
 
-# написать генераторное выражение, которое делает то же самое
+
+def gen(n):
+    """
+    Генераторная функция, которая принимает число N возвращает N рандомных чисел от 1 до 100
+    :param n: размер гененратора
+    :return: генератор случайных чисел
+    """
+    for i in range(n):
+        yield randint(1, 100)
+
+
+# генераторное выражение, которое делает то же самое
+gen_lambda = lambda n: (randint(1, 100) for i in range(n))
+
+
+if __name__ == '__main__':
+    N = 3
+    generator1 = gen(N)
+    assert isinstance(generator1, GeneratorType) and len(list(generator1)) == N
+
+    generator2 = gen_lambda(N)
+    assert isinstance(generator2, GeneratorType) and len(list(generator2)) == N
